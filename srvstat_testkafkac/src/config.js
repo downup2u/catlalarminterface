9f8ed1c2e1f65906a58a20b51be57722244d2cfc
downup2u <xiaoqingwang@126.com>
2018-07-06 14:30:22 +0800

    ==

--- srvstat_testkafkac/src/config.js
+++ srvstat_testkafkac/src/config.js
@@ -1,10 +1,11 @@
 const moment = require('moment');
+const nodeid = process.env.NodeID || 1;
 const config =  {
   mongodburl:process.env.MONGO_URL || 'mongodb://localhost/bmskafka',
   mongos:process.env.mongos==='true'?true:false,
   kafka_cconfig1:{
       'group.id': process.env.GroupId ||'catlpushalarmgrouptest',
-      'metadata.broker.list': process.env.KAFKA_HOST_C || '192.168.2.11:9092,192.168.2.12:9092,192.168.2.13:9092',
+      'metadata.broker.list': process.env.KAFKA_HOST_C || '172.26.175.131:9092,172.26.175.132:9092,172.26.175.133:9092',
       'client.id':`c${process.pid}_${nodeid}`,
       'partition.assignment.strategy':'roundrobin',
       'enable.auto.commit': false
